f553c71ba7dd5944c89fa177a15fb2a7cbaf4ad7
Silvio Frischknecht <github@lupux.ch>
2024-02-10 15:25:49 +0100

    ..

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-export HOST_PREFIX="distx.com+2024-"
+export HOST_PREFIX="distx.org+2024-"
 export ZWOLF="/_zwolf"
 
 git clone "https://github.com/yokto/openlibm.git" openlibm-src
@@ -23,3 +23,35 @@ cp -r xlibc/include "/_zwolf/${HOST_PREFIX}xlibc-common/include"
 (cd llvm-src; ../llvm/build-runtime.sh)
 
 (cd llvm-src; ../llvm/build-zwolf.sh)
+
+mkdir -p "${ZWOLF}/old"
+mv ${ZWOLF}/${HOST_PREFIX}llvm-{common,dev-*,doc-*,x86_64} ${ZWOLF}/old
+mv ${ZWOLF}/tmp/_zwolf/* ${ZWOLF}
+
+(cd ${ZWOLF};
+for x in ${HOST_PREFIX}*
+do
+	tar cf "$x.tar.xz" "$x"
+done
+)
+
+FTP_SERVER="ftpupload.net"
+FTP_USERNAME="if0_35948865"
+
+# Remote directory to upload to
+REMOTE_DIR="/htdocs"
+
+(cd ${ZWOLF};
+for LOCAL_FILE in ${HOST_PREFIX}*tar.xz
+do
+	
+ftp -n $FTP_SERVER <<END_SCRIPT
+quote USER $FTP_USERNAME
+quote PASS $FTP_PASSWORD
+cd $REMOTE_DIR
+put $LOCAL_FILE
+quit
+END_SCRIPT
+
+done
+)
