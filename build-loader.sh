d79f2656122d5b6a88bb655c2a1db54c17a2e922
Silvio Frischknecht <github@lupux.ch>
2024-02-11 17:26:53 +0100

    ci for loader

---
+++ build-loader.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export HOST_PREFIX="distx.org_2024-"
+
+type x86_64-w64-mingw32-gcc
+
+cd zwolf
+make
+
+cd _zwolf
+tar cf "${HOST_PREFIX}xload-x86_64.tar.xz" "${HOST_PREFIX}xload-x86_64"
+
+set +x
+s3cmd --access_key AKIA3UPFWFC4ANF5H3TC --secret_key "${AWS_KEY}" put "${HOST_PREFIX}xload-x86_64.tar.xz" s3://distx
+set -x
